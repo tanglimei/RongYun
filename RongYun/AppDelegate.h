c34ff7f47ba64a4af8836210534c5da7bcfbd9e7
GITang <562630424@qq.com>
2015-12-24 22:07:46 +0800

    Initial Commit

---
+++ RongYun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RongYun
+//
+//  Created by tlm on 15/12/24.
+//  Copyright © 2015年 Genus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
